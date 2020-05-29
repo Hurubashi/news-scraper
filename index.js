b20f7a23f971a6d6deb323c119c792fb852a6d2c
Maxym Rybak <hurubashi@gmail.com>
2020-05-29 13:51:56 +0300

    install donenv, created config/.env

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const express = require('express')
 const app = express()
+const dotenv = require('dotenv')
+
+dotenv.config({ path: './config/.env' })
 const databaseIni = require('./config/databaseIni')
 
 databaseIni()
 
 // Route files
 const crawler = require('./routes/crawler')
-
 app.use('/crawler', crawler)
 
 const PORT = process.env.PORT || 5000
-
 const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
