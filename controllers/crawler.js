7bfd10cde80f0637e1caddbdaaaab53e5da65184
Maxym Rybak <hurubashi@gmail.com>
2020-05-29 17:08:12 +0300

    clean up controller

--- controllers/crawler.js
+++ controllers/crawler.js
@@ -11,12 +11,8 @@ exports.getAinNews = async (req, res, next) => {
 	let createdNews
 	try {
 		createdNews = await db.News.bulkCreate(missingNews)
-	} catch (e) {
-		return res.status(500).json(e)
-	}
-	try {
 		const newsImages = await CrawlerActions.getNewsImagesData(createdNews, missingNews)
-		let createdImgs = await db.NewsImage.bulkCreate(newsImages)
+		await db.NewsImage.bulkCreate(newsImages)
 		const lastNews = await db.News.findAll({
 			limit: CrawlerActions.newsToLoad,
 			include: [db.NewsImage],
@@ -25,5 +21,4 @@ exports.getAinNews = async (req, res, next) => {
 	} catch (e) {
 		return res.status(500).json(e)
 	}
-	return res.status(200).json(createdNews)
 }
